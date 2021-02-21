7d1540d1f0f1e1e31d3277001b6dad278a8cfee8
Debanshu Datta <debanshudatta123@gmail.com>
2021-02-22 01:01:47 +0530

    Dao and Room Setup

--- app/src/main/java/com/debanshu777/newsapp/Article.kt
+++ app/src/main/java/com/debanshu777/newsapp/Article.kt
@@ -1,6 +1,12 @@
 package com.debanshu777.newsapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int?=null,
     val author: String,
     val content: String,
     val description: String,
---
+++ app/src/main/java/com/debanshu777/newsapp/db/ArticleDao.kt
@@ -0,0 +1,17 @@
+package com.debanshu777.newsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.debanshu777.newsapp.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article:Article):Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles():LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
