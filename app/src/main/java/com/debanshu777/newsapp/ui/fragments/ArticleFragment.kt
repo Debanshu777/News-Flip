72806a5d80f3c4521d58e75c42a057ee13436906
Debanshu Datta <debanshudatta123@gmail.com>
2021-04-29 19:10:58 +0530

    Fore dark mode in android webView

--- app/src/main/java/com/debanshu777/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/debanshu777/newsapp/ui/fragments/ArticleFragment.kt
@@ -2,9 +2,15 @@ package com.debanshu777.newsapp.ui.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebViewClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
+import androidx.webkit.WebViewFeature
 import com.debanshu777.newsapp.R
 import com.debanshu777.newsapp.ui.NewsActivity
 import com.debanshu777.newsapp.ui.NewsViewModel
@@ -13,6 +19,16 @@ import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment:Fragment(R.layout.fragment_article){
 
+    private class MyWebViewClient(private val assetLoader: WebViewAssetLoader) :
+        WebViewClientCompat() {
+        override fun shouldInterceptRequest(
+            view: WebView,
+            request: WebResourceRequest
+        ): WebResourceResponse? {
+            return assetLoader.shouldInterceptRequest(request.url)
+        }
+    }
+
     lateinit var viewModel: NewsViewModel
     private val args:ArticleFragmentArgs by navArgs()
 
@@ -20,8 +36,26 @@ class ArticleFragment:Fragment(R.layout.fragment_article){
         super.onViewCreated(view, savedInstanceState)
         viewModel=(activity as NewsActivity).viewModel
         val article=args.article
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            WebSettingsCompat.setForceDark(
+                webView.settings,
+                WebSettingsCompat.FORCE_DARK_ON
+            )
+        }
+        val assetLoader = WebViewAssetLoader.Builder()
+            .setDomain("raw.githubusercontent.com")
+            .addPathHandler(
+                "/views-widgets-samples/assets/",
+                WebViewAssetLoader.AssetsPathHandler(requireContext())
+            )
+            .addPathHandler(
+                "/views-widgets-samples/res/",
+                WebViewAssetLoader.ResourcesPathHandler(requireContext())
+            )
+            .build()
+
         webView.apply {
-            webViewClient= WebViewClient()
+            webViewClient= MyWebViewClient(assetLoader)
             loadUrl(article.url)
         }
         fab.setOnClickListener{
