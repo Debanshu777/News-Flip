460b055dfb731da681c70c6fd80566912a672303
Debanshu Datta <debanshudatta123@gmail.com>
2021-05-19 01:25:40 +0530

    Added debug-apk

--- app/src/main/jni/api-keys.c
+++ app/src/main/jni/api-keys.c
@@ -2,5 +2,5 @@
 
 JNIEXPORT jstring JNICALL
 Java_com_debanshu777_newsapp_util_Constant_00024Companion_getKeys(JNIEnv *env, jobject thiz) {
-    return (*env)->NewStringUTF(env, "7dc8a54d3af244cbb8a4a21651a49b1c");
+    return (*env)->NewStringUTF(env, "YOUR-API-KEY");
 }
